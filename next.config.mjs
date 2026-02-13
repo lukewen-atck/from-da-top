a6c73e29635027e18f9ae4a66783deb22989242b
Shang Lei Wen <lukewen@projectartichoke.co>
2026-02-13 17:52:14 +0800

    fix: Remove fdt-black-card and update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        serverActions: true,
-    },
 };
 
 export default nextConfig;
